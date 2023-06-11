15e1e9fac53a20e5e51c5f40df8a65055449e7a5
subhojit174 <subhojit174@gmail.com>
2023-06-11 23:23:31 +0530

    Updating Objectmapper issue

--- src/main/java/com/retail/retailChain/controller/KafkaController.java
+++ src/main/java/com/retail/retailChain/controller/KafkaController.java
@@ -1,6 +1,7 @@
 package com.retail.retailChain.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.retail.retailChain.entity.RetailStore;
 @EnableKafka
 @RestController
@@ -24,8 +27,15 @@ public List<RetailStore> consumeJsonMsg(){
 @KafkaListener(groupId="group1",topics="${spring.kafka.topic-name}",
 containerFactory = "retailListKafkaListenerContainerFactory")
 public List<RetailStore> getJsonMessageFromTopic(List<RetailStore> retailStoreList){
-	//retailStoreList.stream().forEach((retailStore)->System.out.println(retailStore.getStoreId()));
-	this.retailStoreList=retailStoreList;
+	System.out.println(retailStoreList);
+	ObjectMapper mapper = new ObjectMapper();
+	mapper.findAndRegisterModules();
+	List<RetailStore> convertedRetailStoreList = mapper.convertValue(
+			retailStoreList,
+		    new TypeReference<List<RetailStore>>() { });
+	convertedRetailStoreList.stream().forEach(System.out::println);
+	
+	this.retailStoreList=convertedRetailStoreList;
 	return this.retailStoreList;
 }
 }
